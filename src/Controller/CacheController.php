3b42e3a27ca0e584d85d8840c36aec9a51e5a677
Vincent Chalamon <vincentchalamon@protonmail.com>
2020-12-27 21:06:01 +0100

    Add refresh button to refresh cache

---
+++ src/Controller/CacheController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\CacheClearer\Psr6CacheClearer;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Vincent Chalamon <vincentchalamon@gmail.com>
+ */
+final class CacheController
+{
+    /**
+     * @Route("/clear-cache", condition="request.isXmlHttpRequest()", name="app.cache")
+     */
+    public function __invoke(Psr6CacheClearer $poolClearer): Response
+    {
+        $poolClearer->clearPool('cache.repository');
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+}
